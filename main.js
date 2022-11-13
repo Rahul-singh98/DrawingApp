4fa0d03a96744b3ea60d72d9d12f436c1ed1d712
Rahul-singh98 <rajput.rahul8510@gmail.com>
2022-11-13 11:52:07 +0530

    improved

--- main.js
+++ main.js
@@ -9,6 +9,7 @@ const createWindow = ()=>  {
         webPreferences: {
         preload: path.join(__dirname, 'preload.js'),
       },
+        transparent: true,
     })
     win.maximize();
     win.loadFile('index.html');
